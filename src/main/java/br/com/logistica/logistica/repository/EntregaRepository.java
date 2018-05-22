9e872c7093676b271f145bbf71348188cdb026b3
Weverton Reis <weverton.sant.reis@gmail.com>
2018-05-22 16:59:38 -0300

    iniciando a criacao da pagina de entregas

--- src/main/java/br/com/logistica/logistica/repository/EntregaRepository.java
+++ src/main/java/br/com/logistica/logistica/repository/EntregaRepository.java
@@ -3,9 +3,9 @@ package br.com.logistica.logistica.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.logistica.logistica.entity.Cliente;
+import br.com.logistica.logistica.entity.Entrega;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+public interface EntregaRepository extends JpaRepository<Entrega, Long> {
 
 }
