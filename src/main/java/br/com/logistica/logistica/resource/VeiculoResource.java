8d7605c27925e57b8bc7456cbab0d33e775872c1
Weverton Reis <weverton.sant.reis@gmail.com>
2018-06-07 14:42:21 -0300

    Adicionando @CrossOrigin

--- src/main/java/br/com/logistica/logistica/resource/VeiculoResource.java
+++ src/main/java/br/com/logistica/logistica/resource/VeiculoResource.java
@@ -3,6 +3,7 @@ package br.com.logistica.logistica.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class VeiculoResource {
 	@Autowired
 	private VeiculoRepository veiculoRepository;
 	
+	@CrossOrigin
 	@GetMapping("/")
 	public List<Veiculo> list() {
 		return veiculoRepository.findAll();
