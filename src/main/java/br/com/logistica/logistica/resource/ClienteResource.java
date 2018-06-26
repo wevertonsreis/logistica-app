ad2b4de74a337ab34a1b59f1b196ac14406873d2
Weverton Reis <weverton.sant.reis@gmail.com>
2018-06-26 08:38:32 -0300

    criando servico para salvar veiculo e cliente

--- src/main/java/br/com/logistica/logistica/resource/ClienteResource.java
+++ src/main/java/br/com/logistica/logistica/resource/ClienteResource.java
@@ -3,13 +3,19 @@ package br.com.logistica.logistica.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.logistica.logistica.entity.Cliente;
 import br.com.logistica.logistica.repository.ClienteRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/clientes")
 public class ClienteResource {
@@ -20,6 +26,12 @@ public class ClienteResource {
 	@GetMapping("/")
 	public List<Cliente> list() {
 		return clienteRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Cliente> save(@RequestBody Cliente cliente) {
+		clienteRepository.save(cliente);
+		return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
 	}
 	
 }
--- src/main/java/br/com/logistica/logistica/resource/VeiculoResource.java
+++ src/main/java/br/com/logistica/logistica/resource/VeiculoResource.java
@@ -3,14 +3,19 @@ package br.com.logistica.logistica.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.logistica.logistica.entity.Veiculo;
 import br.com.logistica.logistica.repository.VeiculoRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/veiculos")
 public class VeiculoResource {
@@ -18,10 +23,15 @@ public class VeiculoResource {
 	@Autowired
 	private VeiculoRepository veiculoRepository;
 	
-	@CrossOrigin
 	@GetMapping("/")
 	public List<Veiculo> list() {
 		return veiculoRepository.findAll();
+	}
+	
+	@PostMapping("/")
+	public ResponseEntity<Veiculo> save(@RequestBody Veiculo veiculo) {
+		veiculoRepository.save(veiculo);
+		return new ResponseEntity<Veiculo>(veiculo, HttpStatus.CREATED);
 	}
 	
 }
